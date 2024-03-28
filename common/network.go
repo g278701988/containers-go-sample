017a95f4c9009e72e6e827d640254a13a0dfe483
g278701988 <35322051+g278701988@users.noreply.github.com>
2024-03-29 00:03:44 +0800

    Update network.go

--- common/network.go
+++ common/network.go
@@ -18,7 +18,7 @@ func ReadHttpIP(r *http.Request) string {
 	log.Printf("ReadHttpIP X-Real-Ip:%v\n", IPAddress)
 	if IPAddress == "" {
 		IPAddress = r.Header.Get("X-Forwarded-For")
-		IPAddress = strings.Split(IPAddress,",")
+		IPAddress = strings.Split(IPAddress, ",")[0]
 		log.Printf("ReadHttpIP X-Forwarded-For:%v\n", IPAddress)
 	}
 	if IPAddress == "" {
